471652cd3d2dc56794eb8594e7e3899753cbc215
Breno Furtado <brenofurtrodrigues@gmail.com>
2022-10-29 15:30:09 -0300

    enviando novos exercícios

---
+++ Estrutura_repetitiva_8/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int fat = 1;
+		int n = sc.nextInt();
+		
+		for(int i = 1; i < n; i ++) {
+			fat = fat * i;
+		}
+		
+		System.out.println(fat);
+
+		
+		sc.close();
+		     
+	}
+
+}
